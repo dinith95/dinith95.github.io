a5f33961934934c68e2e40f1c763cb13fdea57c0
Dinith Jayabodhi <Dinith.Jayabodhi@cube.global>
2026-07-29 13:03:13 +0530

    organised compoenents

--- dinith-potrofolio/src/App.tsx
+++ dinith-potrofolio/src/App.tsx
@@ -1,5 +1,5 @@
-import Header from './components/Header';
-import Profile from './components/Profile';
+import Header from './components/Header/Header';
+import Profile from './components/Profile/Profile';
 import './App.css';
 
 function App() {
