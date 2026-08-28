9bc8ac19dbcf6b5c68adab5da238fca0866d1baf
Dinith Jayabodhi <Dinith.Jayabodhi@cube.global>
2026-08-28 13:05:40 +0530

    added styling modification in certification pane

--- dinith-potrofolio/src/App.tsx
+++ dinith-potrofolio/src/App.tsx
@@ -1,6 +1,6 @@
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
-import Education from './components/Education/Education';
+import Journey from './components/Journey/Journey';
 import ScrollDownArrow from './components/ScrollDownArrow/ScrollDownArrow';
 import './App.css';
 
@@ -11,7 +11,7 @@ function App() {
       <main className="App-main">
         <Profile />
       </main>
-      <Education />
+      <Journey />
       <ScrollDownArrow />
     </div>
   );
